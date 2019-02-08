5202e5cecccb1aae8407a7a1094bbc11578c8f83
MinyoungKoo <rnalsdud1014@naver.com>
2019-02-08 16:33:56 +0900

    pod install

---
+++ GithubSearch/GithubSearchTests/GithubSearchTests.swift
@@ -0,0 +1,34 @@
+//
+//  GithubSearchTests.swift
+//  GithubSearchTests
+//
+//  Created by 구민영 on 01/02/2019.
+//  Copyright © 2019 구민영. All rights reserved.
+//
+
+import XCTest
+@testable import GithubSearch
+
+class GithubSearchTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
